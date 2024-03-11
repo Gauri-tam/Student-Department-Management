47d4813c0ccad07912f7575f8cdae3ef957d4fbc
gauri <gauri@weetechsolution.com>
2024-03-11 16:58:42 +0530

    creating role base authenticationd and adding permision to them

--- src/main/java/com/StudDept/confoguration/SecurityConfig.java
+++ src/main/java/com/StudDept/confoguration/SecurityConfig.java
@@ -1,22 +1,54 @@
 package com.StudDept.confoguration;
 
+import com.StudDept.component.JwtAuthenticateFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+
+import static com.StudDept.enumarate.Permission.*;
+import static com.StudDept.enumarate.Role.PRINCIPLE;
+import static org.springframework.http.HttpMethod.*;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final AuthenticationProvider authenticationProvider;
+
+    private final JwtAuthenticateFilter jwtAuthenticateFilter;
+
+    private final LogoutHandler logoutHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request->request
-                .requestMatchers("/api/v1/con/**").permitAll()
-                .anyRequest().authenticated());
+                        //.requestMatchers("/api/v1/con/**").permitAll()                                                   // mail url to create end point
+                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers( "/api/v1/pri/**").hasRole(PRINCIPLE.name())
+                        .requestMatchers(GET,"/api/v1/pri/**" ).hasAuthority(PRINCIPLE_READ.name())
+                        .requestMatchers(POST,"/api/v1/pri/**" ).hasAuthority(PRINCIPLE_CREATE.name())
+                        .requestMatchers(PUT,"/api/v1/pri/**" ).hasAuthority(PRINCIPLE_UPDATE.name())
+                        .requestMatchers(DELETE,"/api/v1/pri/**" ).hasAuthority(PRINCIPLE_DELETE.name())
+
+                        .anyRequest().authenticated())
+                .authenticationProvider(authenticationProvider)
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthenticateFilter, UsernamePasswordAuthenticationFilter.class)
+                .logout(logout->logout
+                        .logoutSuccessUrl("/api/v1/logout")
+                        .addLogoutHandler(logoutHandler)
+                        .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext()));
         return http.build();
     }
 }
---
+++ src/main/java/com/StudDept/controller/HOD_Controller.java
@@ -0,0 +1,94 @@
+package com.StudDept.controller;
+
+import com.StudDept.entity.Department;
+import com.StudDept.entity.Student;
+import com.StudDept.services.DepartmentServices;
+import com.StudDept.services.StudentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/hod")
+@RequiredArgsConstructor
+public class HOD_Controller {
+
+    private final DepartmentServices departmentServices;
+
+    private final StudentService studentService;
+
+    @PostMapping("/dept")
+    public ResponseEntity<String> createDept(@RequestBody Department department){
+        String status = departmentServices.create(department);
+        return new ResponseEntity<>(status, HttpStatus.OK);
+    }
+
+    @PutMapping("/dept/{id}")
+    public ResponseEntity<String> updateDept(@PathVariable("id") long id, @RequestBody Department department){
+        String status = departmentServices.update(id, department);
+        return new ResponseEntity<>(status, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/get/dept")
+    public ResponseEntity<Page<Department>> getAllDept(@RequestParam(value = "No.", defaultValue = "0", required = true) Integer pageNo,
+                                                       @RequestParam(value = "Size", defaultValue = "5", required = true) Integer pageSize ){
+        Pageable pageable = PageRequest.of(pageNo, pageSize);
+        var status = departmentServices.getAll(pageable);
+        return new ResponseEntity<>(status, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/dept")
+    public ResponseEntity<Page<Department>> getBydept(@RequestParam(value = "No", defaultValue = "0", required = true) Integer pageNo,
+                                                      @RequestParam(value = "Size", defaultValue = "5", required = true) Integer pageSize,
+                                                      @RequestParam(value = "studName", defaultValue = "Gauri", required = false)String studName){
+        Pageable pageable = PageRequest.of(pageNo, pageSize);
+        Page<Department> status = departmentServices.getByStudName(pageable, studName);
+        return new ResponseEntity<>(status, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/dept/{id}")
+    public ResponseEntity<Department> getById(@PathVariable("id") Long id){
+        Department status = departmentServices.getById(id);
+        return new ResponseEntity<>(status, HttpStatus.CREATED);
+    }
+
+//----------------------------------------------------------
+    @PostMapping("/stud")
+    public ResponseEntity<String> createStud(@RequestBody Student department){
+        String status = studentService.create(department);
+        return new ResponseEntity<>(status, HttpStatus.OK);
+    }
+
+    @PutMapping("/stud/{id}")
+    public ResponseEntity<String> updateStud(@PathVariable("id") long id,@RequestBody Student student){
+        String status = studentService.update(id, student);
+        return new ResponseEntity<>(status, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/get/stud")
+    public ResponseEntity<Page<Student>> getAllStud(@RequestParam(value = "No", defaultValue = "0", required = true) Integer pageNo,
+                                                    @RequestParam(value = "Size", defaultValue = "5", required = true) Integer pageSize ){
+        Pageable pageable = PageRequest.of(pageNo, pageSize);
+        var status = studentService.getAll(pageable);
+        return new ResponseEntity<>(status, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/get")
+    public ResponseEntity<Page<Student>> getStudBy(@RequestParam(value = "No", defaultValue = "0", required = true) Integer pageNo,
+                                                   @RequestParam(value = "Size", defaultValue = "5", required = true) Integer pageSize,
+                                                   @RequestParam(value = "deptName", defaultValue = "Civil", required = true) String deptName){
+        Pageable pageable = PageRequest.of(pageNo, pageSize);
+        Page<Student> status = studentService.getByDeptName(pageable, deptName);
+        return new ResponseEntity<>(status, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/stud/{id}")
+    public ResponseEntity<Student> getStudById(@PathVariable("id") Long id){
+        Student status = studentService.getById(id);
+        return new ResponseEntity<>(status, HttpStatus.CREATED);
+    }
+}
